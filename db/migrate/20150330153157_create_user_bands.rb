ca199c2a814d409bcdff40707ea59742ad26bc77
Andrew Miksch <superandrew@Andrews-MacBook-Pro.local>
2015-03-30 13:06:58 -0400

    Removed user_controller and updated user_bands migration

--- db/migrate/20150330153157_create_user_bands.rb
+++ db/migrate/20150330153157_create_user_bands.rb
@@ -2,7 +2,7 @@ class CreateUserBands < ActiveRecord::Migration
   def change
     create_table :user_bands do |t|
       t.integer :user_id
-      t.intger :band_id
+      t.integer :band_id
       t.timestamps null: false
     end
   end
