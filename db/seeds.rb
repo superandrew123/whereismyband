f566822d713948600034f0b0153c2eda7b74569a
Andrew Miksch <superandrew@Andrews-MacBook-Pro.local>
2015-03-31 15:42:19 -0400

    updated seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,27 +18,22 @@ ARRAY.each do |letter|
     puts artist_name
   # catchin error if facebook page exists
     begin
-      # binding.pry
       artist_fb = FbGraph::Page.fetch(artist_slug, :access_token => ENV["fb_access_token"])
   # if the musician/band page exists persist that to the bands table
       if artist_fb.category == "Musician/band"
   # before persisting to db make sure artist have a musician/band page
         band = Band.find_or_create_by(name: artist_name)
-        binding.pry
-        if artist_fb.events.length > 0
-          binding.pry
-          artist_fb.events.each do |event|
-            binding.pry
+        # if the band has any upcoming events, save the events
+        if artist_fb.events.length > 0 
+          artist_fb.events.each do |event|    
             show = Event.find_or_create_by(location: event.name, start_time: event.start_time)
             show.link = "http://www.facebook.com/#{artist_slug}/events"
-            show.save
-            binding.pry
+            show.save    
             band.events.push(show)
           end
         end
         band.save
       end
-
     rescue
   # No action needed if no FB page is found
     end
