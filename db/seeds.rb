8d8715997e403d11404560b3ac8e752275c81795
cyzanfar <cghazanfar10@gmail.com>
2015-03-31 11:32:06 -0400

    configure seed for band

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,13 +1,38 @@
-Band.all.each do |band|
-  name = band.name.gsub(" ", "")
-  shows = FbGraph::Page.fetch(name, :access_token => ACCESS_TOKEN).events
-  shows.each do |show|
-    # binding.pry
-    event = Event.find_or_create_by(location: show.name, start_time: show.start_time)
-    event.link = "http://www.facebook.com/#{name}/events"
-    event.save
+require 'open-uri'
+#-----------------------------------------------------
+# This seed task will do our initial storing of artist
+# and events on our band and events table.
+#-----------------------------------------------------
+ACCESS_TOKEN = "800988173324571|81u92rE9wxm9MRLp93No22qKqTM"
+valid_artist_array = Array.new
+invalid_artist_array = Array.new
+("a".."z").to_a.push("number").each do |letter|
+  # Nokogiri object for http://www.billboard.com/artists/(letter)
+  billboard = Nokogiri::HTML(open("http://www.billboard.com/artists/#{letter}"))
+  # iterating through Nokogiri object and getting artist name
+  billboard.css('.view-content .field-content a').each do |artist|
+    artist_slug = artist.text.gsub(" ","")
+    artist_name = artist.text
+  # catchin error if facebook page exists
+    begin
+      artist_fb = FbGraph::Page.fetch(artist_slug, :access_token => ACCESS_TOKEN)
+  # if the musician/band page exists persist that to the bands table
+      if artist_fb.category == "Musician/band"
+  # before persisting to db make sure artist have a musician/band page
+        band = Band.create(name: artist_name)
+        if artist_fb.events.length > 0
 
-    band.events << event
-    band.save
+          artist_fb.events.each do |event|
+            event = Event.find_or_create_by(location: show.name, start_time: show.start_time)
+            event.link = "http://www.facebook.com/#{name}/events"
+            event.save
+            band.events.push(event)
+          end
+        end
+        band.save
+      end
+
+    rescue
+    end
   end
 end
