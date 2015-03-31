2ff1c1878d0a448a807fb11dd237f3df0b5fc0fc
Andrew Miksch <superandrew@Andrews-MacBook-Pro.local>
2015-03-31 17:04:35 -0400

    Added form for adding a band to a user account

--- app/controllers/bands_controller.rb
+++ app/controllers/bands_controller.rb
@@ -1,7 +1,22 @@
 class BandsController < ApplicationController
 
   def new
+    @band = Band.new
     @user = current_user
   end
 
+
+  def create
+    @band = Band.find_by(band_params)
+    @user = current_user
+    @user.bands << @band
+    @user.save
+    redirect_to root_path
+  end
+
+
+  private
+    def band_params
+      params.require(:band).permit(:name)
+    end
 end
