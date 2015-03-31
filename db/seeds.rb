199921a3b924aaf3685c5c914ad1535cdbb474a7
cyzanfar <cghazanfar10@gmail.com>
2015-03-31 13:09:30 -0400

    fixed seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,15 @@ require 'open-uri'
 ACCESS_TOKEN = "800988173324571|81u92rE9wxm9MRLp93No22qKqTM"
 valid_artist_array = Array.new
 invalid_artist_array = Array.new
-("a".."z").to_a.push("number").each do |letter|
+start = Time.now
+# ("a".."z").to_a.push("number").each do |letter|
+
   # Nokogiri object for http://www.billboard.com/artists/(letter)
-  billboard = Nokogiri::HTML(open("http://www.billboard.com/artists/#{letter}"))
+  billboard = Nokogiri::HTML(open("http://www.billboard.com/artists/a"))
+  i = 0
   # iterating through Nokogiri object and getting artist name
   billboard.css('.view-content .field-content a').each do |artist|
+    i += 1
     artist_slug = artist.text.gsub(" ","")
     artist_name = artist.text
   # catchin error if facebook page exists
@@ -19,14 +23,15 @@ invalid_artist_array = Array.new
   # if the musician/band page exists persist that to the bands table
       if artist_fb.category == "Musician/band"
   # before persisting to db make sure artist have a musician/band page
-        band = Band.create(name: artist_name)
+        band = Band.find_or_create_by(name: artist_name)
         if artist_fb.events.length > 0
 
           artist_fb.events.each do |event|
-            event = Event.find_or_create_by(location: show.name, start_time: show.start_time)
-            event.link = "http://www.facebook.com/#{name}/events"
-            event.save
-            band.events.push(event)
+            show = Event.find_or_create_by(location: event.name, start_time: event.start_time)
+            show.link = "http://www.facebook.com/#{artist_slug}/events"
+            show.save
+            binding.pry if i == 27
+            band.events.push(show)
           end
         end
         band.save
@@ -35,4 +40,5 @@ invalid_artist_array = Array.new
     rescue
     end
   end
-end
+# end
+puts "#{Time.now - start_time}"
