399ebb693581d2e456ea7f9e994ecf7a5c813771
SophieDeBenedetto <sophie.debenedetto@gmail.com>
2015-03-31 22:10:13 -0400

    added and configured action mailer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,23 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+# SMTP settings for gmail
+ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =  {
+    :address        => "smtp.gmail.com",
+    :port       =>  587,
+    :domain       =>  'localhost',
+    :user_name      => ENV["gmail"],
+    :password     =>  ENV["gmail_password"],
+    :authentication   => 'plain',
+    :enable_starttls_auto   => true
+}
+
+
+
+
 end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,22 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # SMTP settings for gmail
+ config.action_mailer.default_url_options = { host: "https://whereismyband.herokuapp.com/" }
+
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings =  {
+    :address        => "smtp.gmail.com",
+    :port       =>  587,
+    :domain       =>  'localhost',
+    :user_name      => ENV["gmail"],
+    :password     =>  ENV["gmail_password"],
+    :authentication   => 'plain',
+    :enable_starttls_auto   => true
+}
+
+
+
 end
